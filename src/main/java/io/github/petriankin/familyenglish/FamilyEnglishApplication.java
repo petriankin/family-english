fdad0024d62c4bfddd1ff1e9379ffc3148ab7db6
sergei_petriankin <Sergei_petriankin@epam.com>
2021-01-15 17:15:54 +0300

    basic spring boot application

---
+++ src/main/java/io/github/petriankin/familyenglish/FamilyEnglishApplication.java
@@ -0,0 +1,13 @@
+package io.github.petriankin.familyenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEnglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEnglishApplication.class, args);
+    }
+
+}
