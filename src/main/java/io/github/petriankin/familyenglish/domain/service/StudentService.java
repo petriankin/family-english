67653b3db262a07b2cd378aff25e037265814d65
sergei_petriankin <Sergei_petriankin@epam.com>
2021-01-19 15:37:37 +0300

    StudentService updated

--- src/main/java/io/github/petriankin/familyenglish/domain/service/StudentService.java
+++ src/main/java/io/github/petriankin/familyenglish/domain/service/StudentService.java
@@ -1,16 +1,19 @@
 package io.github.petriankin.familyenglish.domain.service;
 
 
-import io.github.petriankin.familyenglish.domain.models.Student;
+import io.github.petriankin.familyenglish.api.dto.StudentDto;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StudentService {
-    Student create(Student dto);
+    StudentDto create(StudentDto dto);
 
-    Student getById(UUID studentId);
+    StudentDto getById(UUID studentId);
 
-    Student update(UUID studentId, Student dto);
+    List<StudentDto> findAll();
+
+    StudentDto update(UUID studentId, StudentDto dto);
 
     void delete(UUID studentId);
 }
--- src/main/java/io/github/petriankin/familyenglish/domain/service/impl/StudentServiceImpl.java
+++ src/main/java/io/github/petriankin/familyenglish/domain/service/impl/StudentServiceImpl.java
@@ -1,30 +1,44 @@
 package io.github.petriankin.familyenglish.domain.service.impl;
 
+import io.github.petriankin.familyenglish.api.dto.StudentDto;
+import io.github.petriankin.familyenglish.domain.exception.EntityNotFoundException;
+import io.github.petriankin.familyenglish.domain.mapper.StudentMapper;
 import io.github.petriankin.familyenglish.domain.models.Student;
 import io.github.petriankin.familyenglish.domain.repository.StudentRepository;
 import io.github.petriankin.familyenglish.domain.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
+    private final StudentMapper studentMapper;
 
     @Override
-    public Student create(Student student) {
-        return studentRepository.save(student);
+    public StudentDto create(StudentDto student) {
+        Student savedStudent = studentRepository.save(studentMapper.toEntity(student));
+        return studentMapper.toDto(savedStudent);
     }
 
     @Override
-    public Student getById(UUID studentId) {
+    public StudentDto getById(UUID studentId) {
         return studentRepository.findById(studentId)
-                .orElseThrow(() -> new RuntimeException("Student not found")); // TODO: 16.01.2021 custom ENFE
+                .map(studentMapper::toDto)
+                .orElseThrow(() -> new EntityNotFoundException("Student not found"));
     }
 
     @Override
-    public Student update(UUID studentId, Student dto) {
+    public List<StudentDto> findAll() {
+        return studentMapper.toDtos(studentRepository.findAll());
+    }
+
+    @Override
+    public StudentDto update(UUID studentId, StudentDto dto) {
         throw new UnsupportedOperationException();
     }
 
